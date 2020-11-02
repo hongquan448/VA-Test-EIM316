6c2f880b57ac1281788fc173ab2d146770a617ca
hongquan448 <hongquan448@hotmail.com>
2020-11-02 15:24:51 +0800

    Update EndScene Navigation

--- Assets/Scripts/EndSceneNavigation.cs
+++ Assets/Scripts/EndSceneNavigation.cs
@@ -5,8 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class EndSceneNavigation : MonoBehaviour
 {
-    public static void ReturnToTitle() {
+
+    public GameObject resultDisplayWindow;
+
+    public static void ReturnToTitle()
+    {
         Debug.Log("Load EndScene");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ReturnToTitle();
+        }
+        if (Input.GetKeyDown(KeyCode.Return)|| Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            if (resultDisplayWindow.activeSelf == true)
+            {
+                resultDisplayWindow.SetActive(false);
+            }
+            else
+            {
+                resultDisplayWindow.SetActive(true);
+            }
+        }
     }
 }
